e45d85d8856403f9c721881069c8e1c5b8f5cace
Leonardo Lucena <leorlucena@gmail.com>
2025-01-30 18:19:23 -0300

    learning string functions

---
+++ secao01/strings/index.js
@@ -0,0 +1,17 @@
+let umaString = 'Um texto';
+
+console.log(umaString.concat(' de Leo.')); // concat    
+
+console.log(umaString.indexOf('texto')); // busca na string
+
+console.log(umaString.match(/[a-z]/g));
+
+console.log(umaString.replace('Um', 'Outra'));
+
+console.log(umaString.length); //tamanho
+
+console.log(umaString.slice(2, 5)); //fatiando
+
+console.log(umaString.split('m')); //corta do texto.
+
+console.log(umaString.toLowerCase);
